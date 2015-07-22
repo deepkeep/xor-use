d24e343890fdd8da588164fb650e9a064e15a534
Fredrik Noren <noren@spotify.com>
2015-07-21 23:41:24 -0400

    Init

---
+++ README.md
@@ -0,0 +1,6 @@
+# xor-use
+
+## Usage
+
+1. Download and extract the xor network: `curl -LO www.deepkeep.co/FredrikNoren/xor/package.zip && unzip package.zip -d xor && rm package.zip`
+2. Run the program that uses the network: `th test.lua`
---
+++ test.lua
@@ -0,0 +1,9 @@
+require 'torch'
+require 'nngraph'
+
+local net = torch.load('xor/trained-network.t7')
+
+print('0 XOR 0 = ' .. net:forward(torch.Tensor({ 0, 0 }))[1])
+print('0 XOR 1 = ' .. net:forward(torch.Tensor({ 0, 1 }))[1])
+print('1 XOR 0 = ' .. net:forward(torch.Tensor({ 1, 0 }))[1])
+print('1 XOR 1 = ' .. net:forward(torch.Tensor({ 1, 1 }))[1])
